3cabd67a15dcbac4787efb5adeface159ead8c7f
Romain Mencattini <romain.mencattini@zaclys.net>
2018-12-21 15:47:11 +0100

    change the way to save model

--- glowingspoon/_main.py
+++ glowingspoon/_main.py
@@ -7,12 +7,11 @@ import torch
 def core():
     args = programme_parameters()
 
+    model = ConvNet(args.size_block)
     if args.load_nn:
         print(f"Load model from {args.load_nn}")
-        model = torch.load(args.load_nn)
+        model.load_state_dict(torch.load(args.load_nn))
         model.eval()
-    else:
-        model = ConvNet(args.size_block)
 
     if args.train:
         model = train(model, args)
